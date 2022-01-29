e72167ea6b4dd1f959e0bcaeedf1fe995ffd521d
Jean-ChristopheGDn <jeanchristophe.gendron@gmail.com>
2022-01-29 18:57:53 +0100

    :construction_worker: [Update] function onChangeColor, reset to 0 if hue greater than 360

--- src/app/components/color-palette/color-palette.component.ts
+++ src/app/components/color-palette/color-palette.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-palette.component.scss'],
 })
 export class ColorPaletteComponent implements OnInit {
-  
+
   hue: number = 180;
+  analogueHue: number = this.hue + 30;
+  complementaryHue: number = this.hue + 180;
   saturation: number = 50;
   lightness: number = 50;
-  
+
   clrBase: string = `background-color : hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%)`;
   clrAnalogue: string = `background-color : hsl(${this.hue + 30}, ${this.saturation}%, ${this.lightness}%)`;
   clrComplementary: string = `background-color : hsl(${this.hue + 180}, ${this.saturation}%, ${this.lightness}%)`;
@@ -20,9 +22,17 @@ export class ColorPaletteComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onChangeColors(){
-      this.clrBase = `background-color : hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%)`;
-      this.clrAnalogue = `background-color : hsl(${this.hue + 30}, ${this.saturation}%, ${this.lightness}%)`;
-      this.clrComplementary = `background-color : hsl(${this.hue + 180}, ${this.saturation}%, ${this.lightness}%)`;
-    }
+  onChangeColors(): void {
+    this.analogueHue = this.hue + 30;
+    this.complementaryHue = this.hue + 180;
+    if (this.analogueHue >= 360) {
+      this.analogueHue = this.analogueHue - 360;
+    } 
+    if (this.complementaryHue >= 360) {
+      this.complementaryHue = this.complementaryHue - 360;
+    } 
+    this.clrBase = `background-color : hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%)`;
+    this.clrAnalogue = `background-color : hsl(${this.analogueHue}, ${this.saturation}%, ${this.lightness}%)`;
+    this.clrComplementary = `background-color : hsl(${this.complementaryHue}, ${this.saturation}%, ${this.lightness}%)`;
+  }
 }
